7c40755aa7474d6df7314e6e770e488106766420
UmeshJadhav <umesh.jadhav93@gmail.com>
2024-06-21 08:47:13 +0530

    Create SelectionSort.swift

---
+++ Sorting/SelectionSort.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+//Problem Statement: Given an array of N integers, write a program to implement the Selection sorting algorithm.
+/*
+ Example 1:
+ Input: N = 6, array[] = {13,46,24,52,20,9}
+ Output: 9,13,20,24,46,52
+ Explanation: After sorting the array is: 9, 13, 20, 24, 46, 52
+
+ Example 2:
+ Input: N=5, array[] = {5,4,3,2,1}
+ Output: 1,2,3,4,5
+ Explanation: After sorting the array is: 1, 2, 3, 4, 5
+ */
+func selectionSort(_ inputArray: [Int]) {
+    var a = inputArray
+    //Array iteration
+    for i in 0..<a.count - 1 {
+        var minIndex = i
+        //Finding minimum number index from i+1 to n
+        for j in (i + 1)..<a.count {
+            if a[j] < a[minIndex] {
+                minIndex = j
+            }
+        }
+        //If found mininimum number index is not same as i then swapping minimumb number index with i
+        if minIndex != i {
+            let temp = a[i]
+            a[i] = a[minIndex]
+            a[minIndex] = temp
+        }
+    }
+    print(a)
+}
+selectionSort([13,46,24,52,20,9])
