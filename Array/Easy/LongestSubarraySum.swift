d462c095ba049b14aea47e09d432a5920d3dc385
UmeshJadhav <umesh.jadhav93@gmail.com>
2024-07-12 08:58:21 +0530

    Added optimal solution

--- Array/Easy/LongestSubarraySum.swift
+++ Array/Easy/LongestSubarraySum.swift
@@ -11,6 +11,13 @@ Example 2:
 Input Format: N = 5, k = 10, array[] = {2,3,5,1,9}
 Result: 3
 Explanation: The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
+
+ Example 3:
+ Input Format
+ : N = 3, k = 1, array[] = {-1, 1, 1}
+ Result:
+  3
+
 */
 
 //Time Complexity: O(N2)
@@ -41,5 +48,38 @@ func longestSum(n: Int, k: Int, _ array:[Int]) -> Int {
 }
 
 
-let result = longestSum(n: 5, k: 10, [2,3,5,1,9])
+//let result = longestSum(n: 3, k: 1, [-1, 1, 1])
+//print(result)
+
+//Optimal Approch with Two Pointer approch
+//Time complexity O(n)
+//Space Complexity O(1)
+
+
+func longestSubarrySum(n: Int, k: Int, _ array:[Int]) -> Int {
+
+    var left = 0
+    var right = 0
+    var maxLength = 0
+    var currentSum = 0
+
+    while right < n {
+
+        currentSum += array[right]
+        // While the current sum is greater than K, move the left pointer to the right
+        while currentSum > k && left <= right {
+            currentSum -= array[left]
+            left += 1
+        }
+
+        if currentSum == k {
+            maxLength = max(maxLength, right - left + 1)
+
+        }
+        right += 1
+    }
+
+    return maxLength
+}
+let result = longestSubarrySum(n: 5, k: 10, [2,3,5,1,9])
 print(result)
